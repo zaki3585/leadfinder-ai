edbca9d3b298e6d07dde3eb7d7e6039b10ea97a0
Zaki Ahmad <za4642206@gmail.com>
2026-09-06 21:36:22 +0530

    Fix server port for deployment

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.post("/search-leads", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("LeadFinder AI server running on port 3000");
 });
